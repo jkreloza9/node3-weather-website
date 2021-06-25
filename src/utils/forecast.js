692d0c2c6ed85975d95176d4e3c532622829878d
Justine Kaye <jkreloza9@gmail.com>
2021-06-25 18:16:45 +0800

    Add forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require ('request')
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=e26fb8190b2f0a060d50a0abec89dd31&query=' + latitude + ',' + longitude + '&units=f'
     request({url, json:true }, (error, {body}) => {
@@ -7,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location. Try another location', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0]+'. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out');
+            callback(undefined, body.current.weather_descriptions[0]+'. Hi :( .It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees out');
         }
     })
 }
